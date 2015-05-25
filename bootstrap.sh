5dfec092e24416eeaaa7075f930093440ef94126
Jack Reichert <jack@jackreichert.com>
2015-05-25 12:57:59 -0400

    initial commit

---
+++ bootstrap.sh
@@ -0,0 +1,100 @@
+# percona
+sudo apt-key adv --keyserver keys.gnupg.net --recv-keys 1C4CBDCDCD2EFD2A
+sudo bash -c 'echo deb http://repo.percona.com/apt trusty main >> /etc/apt/sources.list'
+sudo bash -c 'echo deb-src http://repo.percona.com/apt trusty main >> /etc/apt/sources.list'
+sudo apt-get -y update && sudo apt-get -y upgrade
+echo "percona-server-server-5.6 percona-server-server/root_password password root" | sudo debconf-set-selections
+echo "percona-server-server-5.6 percona-server-server/root_password_again password root" | sudo debconf-set-selections
+sudo apt-get install -qq -y percona-server-server-5.6 percona-server-client-5.6
+
+# nodejs 
+sudo apt-get install python-software-properties
+sudo apt-add-repository ppa:chris-lea/node.js
+sudo curl -sL https://deb.nodesource.com/setup | sudo bash -
+
+# nginx
+sudo apt-get install -y nginx
+
+# install php
+sudo apt-get install -y php5-common php5-mysql php5-xmlrpc php5-cgi php5-curl php5-gd php5-cli php5-fpm php-apc php5-dev php5-mcrypt  php5-tidy
+
+# create missing directories
+sudo mkdir -p /var/www/html/play/
+sudo mkdir -p /etc/nginx/global/
+
+# create nginx config files
+(
+sudo cat <<'EOF'
+server {
+	server_name play.lcl;
+	listen   80;
+	root /var/www/html/play;
+	access_log /var/log/nginx/jr-access.log;
+	error_log /var/log/nginx/jr-error.log warn;
+    
+	# Permalink rewrite # comment out if using cache
+	location / {
+		try_files $uri $uri/ /index.php?$args;
+	}
+	include /etc/nginx/global/wordpress.conf;
+}
+EOF
+) > /etc/nginx/conf.d/play.conf
+
+(
+sudo cat <<'EOF'
+# WordPress single blog rules.
+# Designed to be included in any server {} block.
+index   index.php;
+
+# Add trailing slash to */wp-admin requests.
+rewrite /wp-admin$ $scheme://$host$uri/ permanent;
+# Sitemap
+rewrite ^/sitemap_index\.xml$ /index.php?sitemap=1 last;
+rewrite ^/([^/]+?)-sitemap([0-9]+)?\.xml$ /index.php?sitemap=$1&sitemap_n=$2 last;
+# assets
+location ~ ^/asset/(.*)$ {
+	try_files $uri $uri/ /index.php?$query_string;
+}
+
+# Pass all .php files onto a php-fpm/php-fcgi server.
+location ~ \.php$ {
+	try_files $uri =404;
+	include /etc/nginx/fastcgi_params;
+	fastcgi_pass unix:/var/run/php5-fpm.sock;
+	fastcgi_index index.php;
+	fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
+	fastcgi_keep_conn on;
+	fastcgi_buffers 16 16k; 
+	fastcgi_buffer_size 32k;
+}
+EOF
+) > /etc/nginx/global/wordpress.conf
+
+(
+sudo cat << 'EOF'
+<?php phpinfo();
+EOF
+) > /var/www/html/play/index.php
+
+# restart after reconfig
+sudo service nginx restart 
+sudo service php5-fpm restart
+sudo service mysql restart
+
+# git
+sudo apt-get -y install git
+
+# node / npm
+sudo apt-get -y install nodejs
+sudo apt-get -y install build-essential
+cd ~/
+curl -O -L https://npmjs.org/install.sh
+sudo sh install.sh
+
+# devtools
+sudo npm install -g yo
+sudo npm install -g bower
+sudo npm install -g grunt-cli
+
+sudo npm install -g yo generator-wordpress
