2a3ead3ac25a663e8aa20904b56f68f18a8ad57d
Jack Reichert <awesome@jackreichert.com>
2018-05-07 12:21:13 -0400

    update setup sites

--- assets/setup-site.sh
+++ assets/setup-site.sh
@@ -7,7 +7,7 @@ echo '~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~'
 cd /var/www/html
 wp core download
 wp core config --dbname=play_lcl --dbuser=root --dbpass= --dbhost=localhost
-wp db create play_lcl
+wp db create
 wp core install --url=play.lcl --title=Playground --admin_user=admin --admin_password=admin --admin_email=admin@example.com
 
 wp theme update --all
